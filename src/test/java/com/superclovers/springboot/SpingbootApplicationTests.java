359dfbb549909af0df4b5379ca92161741c7148b
Weiyongsen <247797226@qq.com>
2020-10-22 09:04:38 +0800

    update src/test/java/com/superclovers/springboot/SpingbootApplicationTests.java. 修改测试启动类（SpingbootApplicationTests）代码重复

--- src/test/java/com/superclovers/springboot/SpingbootApplicationTests.java
+++ src/test/java/com/superclovers/springboot/SpingbootApplicationTests.java
@@ -1,30 +1,16 @@
 package com.superclovers.springboot;
 
-import com.superclovers.springboot.entity.GoodsEntity;
-import com.superclovers.springboot.mapper.GoodsMapper;
+
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-class GoodsMapperTests {
-
-    @Autowired
-    public GoodsMapper mapper;
+class SpingbootApplicationTests {
 
     @Test
-    public void findList() {
-        System.err.println(mapper);
-
-        List<GoodsEntity> list = mapper.findList();
-        System.err.println("count=" + list.size());
-        for (GoodsEntity item : list) {
-            System.out.println(item);
-        }
+    void contextLoads() {
+        
     }
 }
